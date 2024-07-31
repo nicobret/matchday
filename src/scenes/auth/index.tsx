11ae715736699154987fc215b19a9a617695a35b
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-08-01 00:48:19 +0200

    fix

--- src/scenes/auth/index.tsx
+++ src/scenes/auth/index.tsx
@@ -4,11 +4,7 @@ import { Auth as SupabaseAuth } from "@supabase/auth-ui-react";
 export default function Auth() {
   return (
     <div className="mx-auto max-w-lg p-4">
-      <SupabaseAuth
-        supabaseClient={supabase}
-        providers={[]}
-        redirectTo="/account"
-      />
+      <SupabaseAuth supabaseClient={supabase} providers={[]} />
     </div>
   );
 }
