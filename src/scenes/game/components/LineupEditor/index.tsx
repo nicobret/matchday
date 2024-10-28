4aaefc41d2fdcfb00961309ff8a37504be7a1e1b
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-10-28 02:10:04 +0100

    up

--- src/scenes/game/components/LineupEditor/index.tsx
+++ src/scenes/game/components/LineupEditor/index.tsx
@@ -1,5 +1,5 @@
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
-import { Shirt } from "lucide-react";
+import { Loader, Shirt } from "lucide-react";
 import { Player } from "../../lib/player/player.service";
 import useUpdatePlayer from "../../lib/player/useUpdatePlayer";
 import Team from "./Team";
@@ -19,7 +19,7 @@ export default function LineupEditor({
   players: Player[];
   disabled: boolean;
 }) {
-  const { mutate } = useUpdatePlayer(gameId);
+  const { mutate, isLoading } = useUpdatePlayer(gameId);
   function handleDrop(event: DragEndEvent) {
     if (disabled) {
       window.alert("Vous n'avez pas la permission de modifier l'équipe.");
@@ -39,7 +39,7 @@ export default function LineupEditor({
 
   return (
     <DndContext onDragEnd={handleDrop}>
-      <div className="grid gap-4 md:grid-cols-3">
+      <div className="${isLoading grid gap-4 md:grid-cols-3">
         <Team
           label="Sans équipe"
           icon={<Shirt className="ml-2 inline-block h-5 w-5 text-muted" />}
@@ -61,6 +61,16 @@ export default function LineupEditor({
           players={players.filter((p) => p.team === 1)}
         />
       </div>
+      <p className="mt-4 text-muted-foreground">
+        {isLoading ? (
+          <>
+            <Loader className="mr-2 inline-block h-5 w-5 animate-spin align-text-bottom" />
+            Enregistrement...
+          </>
+        ) : (
+          "Enregistrement automatique."
+        )}
+      </p>
     </DndContext>
   );
 }
