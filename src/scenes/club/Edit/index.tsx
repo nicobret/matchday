44ca7df762f91bb77fae05ca0dcf6d688609a5b5
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-12-13 14:14:07 +0100

    add season

--- src/scenes/club/Edit/index.tsx
+++ src/scenes/club/Edit/index.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { queryClient } from "@/lib/react-query";
 import useSeasons from "@/scenes/club/lib/useSeasons";
+import supabase from "@/utils/supabase";
 import { ArrowLeft } from "lucide-react";
 import { Link, useParams } from "wouter";
 import useClub from "../lib/useClub";
@@ -10,9 +13,18 @@ export default function EditClub() {
   const { id } = useParams();
   const { data: club } = useClub(Number(id));
   const { data: seasons } = useSeasons(Number(id));
+
   if (!club || !seasons) {
     return <p className="animate_pulse text-center">Chargement...</p>;
   }
+
+  async function handleCreateSeason() {
+    await supabase
+      .from("season")
+      .insert({ club_id: club!.id, name: "Nouvelle saison" });
+    queryClient.invalidateQueries("seasons");
+  }
+
   return (
     <div className="mx-auto max-w-5xl p-4">
       <Link to={`/club/${id}`} className="text-sm text-muted-foreground">
@@ -31,6 +43,11 @@ export default function EditClub() {
         <h2 className="mb-4 text-2xl font-semibold uppercase tracking-tight">
           Liste des saisons
         </h2>
+        <Button onClick={handleCreateSeason} variant="secondary">
+          Ajouter une saison
+        </Button>
+        <br />
+        <br />
         <SeasonList clubId={club.id} seasons={seasons} />
       </section>
 
