df33646b85abd9e828cf0df49fb7c0939b6b0acf
nicobret <nicolas.bretecher@proton.me>
2025-02-15 18:03:33 +0100

    up

---
+++ src/scenes/club/components/EventStats.tsx
@@ -0,0 +1,100 @@
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Crown } from "lucide-react";
+import { useQueryState } from "nuqs";
+import { ClubStatsType } from "../lib/club/useClubStats";
+
+const sortEventsOptions = [
+  { value: "goals", label: "Buts" },
+  { value: "assists", label: "Passes décisives" },
+  { value: "saves", label: "Arrêts" },
+];
+
+type SortEventsBy = "goals" | "assists" | "saves";
+
+export default function EventStats({ data }: { data: ClubStatsType[] }) {
+  const [sortEventsBy, setSortEventsBy] = useQueryState<SortEventsBy>(
+    "sortEventsBy",
+    {
+      defaultValue: "goals",
+      clearOnDefault: false,
+      parse: (value) => value as SortEventsBy,
+    },
+  );
+
+  return (
+    <div className="overflow-x-auto">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Actions
+      </h3>
+      <div className="mt-4 mb-2 flex gap-2">
+        <div className="grid w-full max-w-36 items-center gap-1.5">
+          <Label>Trier par</Label>
+          <Select
+            name="sortEventsBy"
+            value={sortEventsBy}
+            onValueChange={(value: SortEventsBy) => setSortEventsBy(value)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Trier par" />
+            </SelectTrigger>
+            <SelectContent>
+              {sortEventsOptions.map((option) => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableRow></TableRow>
+            <TableHead>Prénom</TableHead>
+            <TableHead>Buts</TableHead>
+            <TableHead>Passes décisives</TableHead>
+            <TableHead>Arrêts</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data
+            .sort((a, b) => b[sortEventsBy] - a[sortEventsBy])
+            .map((row, index) => {
+              return (
+                <TableRow key={row.user_id}>
+                  <TableCell className="text-center">
+                    {index + 1 === 1 ? (
+                      <Crown className="text-primary inline-block" />
+                    ) : (
+                      <span className="text-muted-foreground">{index + 1}</span>
+                    )}
+                  </TableCell>
+                  <TableCell>{row.firstname}</TableCell>
+                  <TableCell>{row.goals}</TableCell>
+                  <TableCell>{row.assists}</TableCell>
+                  <TableCell>{row.saves}</TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
---
+++ src/scenes/club/components/WinStats.tsx
@@ -0,0 +1,103 @@
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Crown } from "lucide-react";
+import { useQueryState } from "nuqs";
+import { ClubStatsType } from "../lib/club/useClubStats";
+
+const sortWinsOptions = [
+  { value: "games", label: "Matchs" },
+  { value: "wins", label: "Victoires" },
+  { value: "winstreak", label: "Série de victoires" },
+  { value: "winrate", label: "Pourcentage de victoires" },
+];
+
+type SortWinsBy = "games" | "wins" | "winstreak" | "winrate";
+
+export default function WinStats({ data }: { data: ClubStatsType[] }) {
+  const [sortWinsBy, setSortWinsBy] = useQueryState<SortWinsBy>("sortWinsBy", {
+    defaultValue: "wins",
+    clearOnDefault: false,
+    parse: (value) => value as SortWinsBy,
+  });
+
+  return (
+    <div className="overflow-x-auto">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Victoires
+      </h3>
+      <div className="mt-4 mb-2 flex gap-2">
+        <div className="grid w-full max-w-36 items-center gap-1.5">
+          <Label>Trier par</Label>
+          <Select
+            name="sortEventsBy"
+            value={sortWinsBy}
+            onValueChange={(value: SortWinsBy) => setSortWinsBy(value)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Trier par" />
+            </SelectTrigger>
+            <SelectContent>
+              {sortWinsOptions.map((option) => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableRow></TableRow>
+            <TableHead>Prénom</TableHead>
+            <TableHead>Matchs</TableHead>
+            <TableHead>Victoires</TableHead>
+            <TableHead>Série</TableHead>
+            <TableHead>Pourcentage</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data
+            .sort(
+              (a: ClubStatsType, b: ClubStatsType) =>
+                b[sortWinsBy] - a[sortWinsBy],
+            )
+            .map((row, index) => {
+              return (
+                <TableRow key={row.user_id}>
+                  <TableCell className="text-center">
+                    {index + 1 === 1 ? (
+                      <Crown className="text-primary inline-block" />
+                    ) : (
+                      <span className="text-muted-foreground">{index + 1}</span>
+                    )}
+                  </TableCell>
+                  <TableCell>{row.firstname}</TableCell>
+                  <TableCell>{row.games}</TableCell>
+                  <TableCell>{row.wins}</TableCell>
+                  <TableCell>{row.winstreak}</TableCell>
+                  <TableCell>{row.winrate} %</TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
